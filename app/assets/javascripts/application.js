0ee89ffab3fc3445b248f61fd55d660fd5c531e8
Tanner Harman <harmtan2@gmail.com>
2018-10-10 16:25:58 -0600

    Reorderd requirements

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,8 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//= require jquery3
+//= require jquery
+//= require bootstrap-sprockets
 //= require rails-ujs
 //= require jquery-ui
 //= require html.sortable
@@ -18,4 +19,3 @@
 //= require cable
 //= require turbolinks
 //= require_tree .
-//= require bootstrap-sprockets
