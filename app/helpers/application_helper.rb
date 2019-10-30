521bf20041daf7814a9072c9593d972c178283d2
Tanner Harman <harmtan2@gmail.com>
2019-10-30 13:30:35 -0600

    Re-Applied helpers for Navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,4 @@
 module ApplicationHelper
-
   class CodeRayify < Redcarpet::Render::HTML
     def block_code(code, language)
         CodeRay.scan(code, language).div
@@ -21,18 +20,14 @@ module ApplicationHelper
   end
   
   def login_helper style = ''
-    if current_user.is_a?(GuestUser)
-      (link_to "Register", new_user_registration_path, class: style) +
-      " ".html_safe +
-      (link_to "Login", new_user_session_path, class: style)
-    else
-      link_to "Logout", destroy_user_session_path, method: :delete, class: style
+    if !current_user.is_a?(GuestUser)
+      content_tag(:li, link_to("Logout", destroy_user_session_path, method: :delete, class: style))
     end
   end
 
   def source_helper(styles)
     if session[:source]
-      greeting = "Thanks for visiting me from #{session[:source]}, please #{link_to 'contact me', contact_path} if you'd like to work together"
+      greeting = "Thanks for visiting me from #{session[:source]}, please #{link_to('contact me', contact_path)} if you'd like to work together"
       content_tag(:div, greeting.html_safe, class: styles)
     end
   end
