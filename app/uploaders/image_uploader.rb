9283e441ebde5bf8c2eac53780979e7906de636c
Tanner Harman <harmtan2@gmail.com>
2019-11-22 11:27:48 -0700

    refactored image uploader to be usabel for entire app

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,5 +1,10 @@
-class PortfolioUploader < CarrierWave::Uploader::Base
+class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  process resize_to_fit: [1000, 1000]
+  
   storage :aws
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
