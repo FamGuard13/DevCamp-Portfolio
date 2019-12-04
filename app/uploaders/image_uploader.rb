be1931a36729bdad2bb0e4f8e9fc4c8d8921b8c9
Tanner Harman <harmtan2@gmail.com>
2019-12-04 13:21:51 -0700

    Changed uploader from File to AWS

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,7 +3,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   process resize_to_fit: [1000, 1000]
 
-  storage :file
+  storage :aws
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,6 @@ module DevcampPortfolio
     config.generators.system_tests = nil
     config.eager_load_paths << "#{Rails.root}/lib"
     config.secret_key_base = ENV["SECRET_KEY_BASE"]
-    config.serve_static_assets = true
     config.generators.javascript_engine = :js
   end
 end
